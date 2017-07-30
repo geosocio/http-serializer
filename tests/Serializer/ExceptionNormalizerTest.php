f10857c626be50637929375d13fc4df36f5021a7
David Barratt <david@davidwbarratt.com>
2017-07-30 13:06:40 -0400

    Add Exception Normalizer Test

--- tests/Serializer/ExceptionNormalizerTest.php
+++ tests/Serializer/ExceptionNormalizerTest.php
@@ -2,6 +2,8 @@
 
 namespace GeoSocio\HttpSerializer\Serializer;
 
+use GeoSocio\HttpSerializer\Exception\ConstraintViolationException;
+use GeoSocio\HttpSerializer\Exception\ConstraintViolationExceptionInterface;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
@@ -34,6 +36,22 @@ class ExceptionNormalizerTest extends TestCase
         $this->assertArrayHasKey('previous', $result);
         $this->assertNull($result['previous']);
         $this->assertArrayHasKey('trace', $result);
+    }
+
+    /**
+     * Test Supports Normalization.
+     */
+    public function testNormalizeConstraintViolationException()
+    {
+        $normalizer = new ExceptionNormalizer();
+
+        $main = $this->createMock(NormalizerInterface::class);
+        $normalizer->setNormalizer($main);
+
+        $exception = new ConstraintViolationException();
+
+        $result = $normalizer->normalize($exception);
+        $this->assertArrayHasKey('constraintViolations', $result);
     }
 
     /**
