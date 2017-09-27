5f9aa3c8f03ad6961d2db10d74bd504007901531
David Barratt <david@davidwbarratt.com>
2017-09-26 23:08:59 -0400

    Fix test coverage

--- tests/GroupResolver/Request/GroupResolverManagerTest.php
+++ tests/GroupResolver/Request/GroupResolverManagerTest.php
@@ -1,19 +1,29 @@
 <?php
 
-namespace GeoSocio\HttpSerializer\GroupResolver\Response;
+namespace GeoSocio\HttpSerializer\GroupResolver\Request;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Group Resolver Manager.
  */
 class GroupResolverManagerTest extends TestCase
 {
-    public function testAddResolver()
+    /**
+     * Test resolve.
+     */
+    public function testResolve()
     {
         $manager = new GroupResolverManager();
 
         $resolver = $this->createMock(GroupResolverInterface::class);
         $this->assertSame($manager, $manager->addResolver($resolver));
+
+        $request = $this->getMockBuilder(Request::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->assertEmpty($manager->resolve($request, \stdClass::class));
     }
 }
---
+++ tests/GroupResolver/Response/GroupResolverManagerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace GeoSocio\HttpSerializer\GroupResolver\Response;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Group Resolver Manager.
+ */
+class GroupResolverManagerTest extends TestCase
+{
+    /**
+     * Test resolve.
+     */
+    public function testResolve()
+    {
+        $manager = new GroupResolverManager();
+
+        $resolver = $this->createMock(GroupResolverInterface::class);
+        $this->assertSame($manager, $manager->addResolver($resolver));
+
+        $this->assertEmpty($manager->resolve(new \stdClass()));
+    }
+
+    /**
+     * Test resolve.
+     */
+    public function testSupports()
+    {
+        $manager = new GroupResolverManager();
+
+        $resolver = $this->createMock(GroupResolverInterface::class);
+        $this->assertSame($manager, $manager->addResolver($resolver));
+
+        $this->assertFalse($manager->supports(new \stdClass()));
+    }
+}
