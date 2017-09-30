105a2442f124b79aad026b170542666c0a2b6c7e
David Barratt <david@davidwbarratt.com>
2017-09-30 11:53:35 -0400

    Add @Annotation to the Annotations to prevent an exception

--- src/Annotation/ResponseGroups.php
+++ src/Annotation/ResponseGroups.php
@@ -4,6 +4,12 @@ namespace GeoSocio\HttpSerializer\Annotation;
 
 use Symfony\Component\Serializer\Annotation\Groups;
 
+/**
+ * Annotation class for @ResponseGroups().
+ *
+ * @Annotation
+ * @Target({"METHOD"})
+ */
 class ResponseGroups extends Groups
 {
 
