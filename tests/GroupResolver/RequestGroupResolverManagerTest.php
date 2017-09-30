03f58ca94504989b9711f98281872b01cfa8aa4d
David Barratt <david@davidwbarratt.com>
2017-09-30 11:03:10 -0400

    Fix test coverage

--- tests/GroupResolver/RequestGroupResolverManagerTest.php
+++ tests/GroupResolver/RequestGroupResolverManagerTest.php
@@ -26,4 +26,25 @@ class RequestGroupResolverManagerTest extends TestCase
 
         $this->assertEmpty($manager->resolve($request, \stdClass::class));
     }
+
+    /**
+     * Test resolve.
+     */
+    public function testSupports()
+    {
+        $manager = new RequestGroupResolverManager();
+
+        $resolver = $this->createMock(RequestGroupResolverInterface::class);
+        $resolver->expects($this->once())
+           ->method('supports')
+           ->willReturn(true);
+
+        $this->assertSame($manager, $manager->addResolver($resolver));
+
+        $request = $this->getMockBuilder(Request::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->assertTrue($manager->supports($request, \stdClass::class));
+    }
 }
