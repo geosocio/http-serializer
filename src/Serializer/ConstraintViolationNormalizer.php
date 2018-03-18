82a021ba096999100d86ea0543965233d0c5fac7
David Barratt <david@davidwbarratt.com>
2018-03-18 10:14:36 -0400

    Normalize the propertyPath so it matches the repsonse

--- src/Serializer/ConstraintViolationNormalizer.php
+++ src/Serializer/ConstraintViolationNormalizer.php
@@ -2,24 +2,50 @@
 
 namespace GeoSocio\HttpSerializer\Serializer;
 
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 
 /**
  * Exception Normalizer
  */
-class ConstraintViolationNormalizer implements NormalizerInterface
+class ConstraintViolationNormalizer implements NormalizerInterface, NormalizerAwareInterface
 {
+    use NormalizerAwareTrait;
+
     /**
      * {@inheritdoc}
      */
     public function normalize($object, $format = null, array $context = array())
     {
-        return [
+
+        // Convert the peroperty paths so they match the serialization.
+        $propertyPath = $object->getPropertyPath();
+        if ($this->normalizer && $this->normalizer->nameConverter instanceof NameConverterInterface) {
+            $properties = explode('.', $propertyPath);
+            $properties = array_map(function ($property) {
+                return $this->normalizer->nameConverter->normalize($property);
+            }, $properties);
+            $propertyPath = implode('.', $properties);
+        }
+
+        $data = [
             'message' => $object->getMessage(),
-            'propertyPath' => $object->getPropertyPath(),
+            'propertyPath' => $propertyPath,
             'code' => $object->getCode(),
         ];
+
+        if ($this->normalizer && $this->normalizer->nameConverter instanceof NameConverterInterface) {
+            foreach ($data as $key => $value) {
+                $normalized[$this->normalizer->nameConverter->normalize($key)] = $value;
+            }
+        } else {
+            $normalized = $data;
+        }
+
+        return $normalized;
     }
 
     /**
